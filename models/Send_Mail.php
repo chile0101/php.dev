d747e48eb5776b824c67b8c4395e56c4fd2d61f5
chile <levanchi74@gmail.com>
2019-03-19 00:54:08 +0700

    fix mailer

--- models/Send_Mail.php
+++ models/Send_Mail.php
@@ -2,26 +2,26 @@
 function Send_Mail($to,$subject,$body)
 {
 require 'class.phpmailer.php';
-$from = "chilevan74@gmail.com";
+$from = "noreply@htcstore.com";
 $mail = new PHPMailer();
 $mail->IsSMTP(true); // SMTP
 $mail->SMTPAuth   = true;  // SMTP authentication
 $mail->Mailer = "smtp";
 $mail->Host       = "tls://smtp.gmail.com"; // Amazon SES server, note "tls://" protocol
 $mail->Port       = 465;                    // set the SMTP port
-$mail->Username   = "chilevan74@gmail.com";  // SES SMTP  username
-$mail->Password   = "thohuynh1512";  // SES SMTP password
-$mail->SetFrom($from, 'From Name');
-$mail->AddReplyTo($from,'9lessons Labs');
+$mail->Username   = "anphawolf@gmail.com";  // SES SMTP  username
+$mail->Password   = "anphawolf@%)$1997";  // SES SMTP password
+$mail->SetFrom($from, 'HTCstore');
+//$mail->AddReplyTo($from,'HTC Store');
 $mail->Subject = $subject;
 $mail->MsgHTML($body);
 $address = $to;
 $mail->AddAddress($address, $to);
 
 if(!$mail->Send())
-return false;
+    return false;
 else
-return true;
+    return true;
 
 }
 ?>
