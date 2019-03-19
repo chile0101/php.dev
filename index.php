99b01bb38b9e2d0198786da1df322fcb38071e38
chile <levanchi74@gmail.com>
2019-03-19 09:29:44 +0700

    fix mailer

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ require('config.php');
 ////////////////////
 
 //require('mailer/Send_Mail.php');
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// require 'PHPMailer/src/Exception.php';
-// require 'PHPMailer/src/PHPMailer.php';
-// require 'PHPMailer/src/SMTP.php';
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
 //////
 
 require('classes/Bootstrap.php');  
