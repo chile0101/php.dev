9c5c446f8b80da8ec7684edae853737f72f3e031
chile <levanchi74@gmail.com>
2019-04-03 22:42:11 +0700

    fix config

--- views/home/contact.php
+++ views/home/contact.php
@@ -1,12 +1,5 @@
 
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-</head>
-
-
 <?php 
   $emailErr="";
   if(isset($_POST['contact_us'])){
