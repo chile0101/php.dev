5f3028e688ae6cf57e99fb1c488b230ab23185fb
chile <levanchi74@gmail.com>
2019-04-03 21:59:33 +0700

    fix config

--- config.php
+++ config.php
@@ -5,11 +5,11 @@ define("DB_HOST","localhost");
 define("DB_USER","root");
 define("DB_PASS","");
 define("DB_NAME","db_phpdev");
-define("CHARSET","utf8");
+// define("CHARSET","utf8");
 
 // Define URL
 
-define("ROOT_PATH","/");
+define("ROOT_PATH","./");
 define("ROOT_URL","/");
 define("ROOT_URL_ADMIN","/admin/");
 
