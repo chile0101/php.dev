8bd270b3cbec51135867c9f40cea314ce18e2bd5
chile <levanchi74@gmail.com>
2019-04-25 07:48:33 +0700

    fix color

--- views/cart/checkout.php
+++ views/cart/checkout.php
@@ -1,7 +1,7 @@
 
 <div class="container">
 <div class="checkout">
-	<h3>Your order has been processed. Please check your email to get the activation code for the game you paid. 
+	<h3 style="color:green;">Your order has been processed. Please check your email to get the activation code for the game you paid. 
 	Thank you for using our service</h3><br>
 	<a class="btn btn-dark" href="<?php echo ROOT_URL; ?>home/shop">RETURN TO SHOP</a>
 
