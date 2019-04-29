7195dd14c8d60120a2f5780febee5158321c8ae2
chile <levanchi74@gmail.com>
2019-04-29 16:15:33 +0700

    fix search

--- views/layout/main.php
+++ views/layout/main.php
@@ -69,7 +69,25 @@
   </style>
 
   <script>
-    
+    function myFunction() {
+      // Declare variables
+      var input, filter, ul, li, a, i, txtValue;
+      input = document.getElementById('myInput');
+      filter = input.value.toUpperCase();
+      ul = document.getElementById("myUL");
+      li = ul.getElementsByTagName('li');
+
+      // Loop through all list items, and hide those who don't match the search query
+      for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = "";
+        } else {
+          li[i].style.display = "none";
+        }
+      }
+    }
 
   </script>
 
@@ -105,7 +123,7 @@
         </form>
 
         <div class="result-search">
-          <ul>
+          <ul id="myUL">
             <li><a href="#">Adele</a></li>
             <li><a href="#">Agnes</a></li>
 
