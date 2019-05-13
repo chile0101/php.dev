dda1b303e9b865edf2e72ee2e8d4b1ec3df43265
chile <levanchi74@gmail.com>
2019-05-13 11:41:58 +0700

    add analics

--- views/admins/analics/index.php
+++ views/admins/analics/index.php
@@ -1,4 +1,33 @@
 <div>
  
- 
+  <br>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">Total Order</th>
+        
+        <th scope="col">Total Products</th>
+        
+        <th scope="col">Date</th>
+
+     
+      </tr>
+    </thead>
+    <tbody>
+     
+      <tr>
+        <th scope="row"></th>
+       
+        <td><a  class="btn btn-success" href=""><?php echo $viewmodel[1]; ?></a></td>
+        <td><a  class="btn btn-success" href=""><?php echo $viewmodel[2]; ?></td>
+        <td> <a  class="btn btn-success" href=""><?php
+            $today = date("F j, Y, g:i a");     
+            echo $today;
+        
+        ?></a> </td>
+       
+      </tr>
+    
+    </tbody>
+  </table>
 </div>
